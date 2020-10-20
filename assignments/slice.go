be1444802802b4cf6f4de47700235001539eed14
Tanzim Mokammel <tanzim.mokammel@moonfare.com>
2020-10-20 16:54:46 +0200

    fix slice.go

--- assignments/slice.go
+++ assignments/slice.go
@@ -19,15 +19,29 @@ func main() {
 		if val == "X" {
 			break
 		}
-		i, err := strconv.Atoi(val)
+		iVal, err := strconv.Atoi(val)
 		if err != nil {
-			fmt.Println("invalid integer")
+			fmt.Println("Invalid integer")
 		} else {
 			// TODO: fix placement!!
-			if idx < origSize {
-				sli[idx] = i
+			if idx < origSize-1 {
+				sli[idx] = iVal
+			} else if idx == origSize-1 {
+				if iVal != 0 {
+					zeroAt := 0
+					for k := 0; k < origSize; k++ {
+						if sli[k] == 0 {
+							zeroAt = k
+							break
+						}
+					}
+					sli[zeroAt] = iVal
+				} else {
+					// do nothing
+				}
+
 			} else {
-				sli = append(sli, i)
+				sli = append(sli, iVal)
 			}
 			sort.Slice(sli, func(i, j int) bool {
 				return sli[i] < sli[j]
