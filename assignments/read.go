ac326ef12d137eb741c265094217b3c61959a6c4
Tanzim Mokammel <tanzim.mokammel@moonfare.com>
2020-10-21 17:23:08 +0200

    read.go

--- assignments/read.go
+++ assignments/read.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 type person struct {
@@ -11,24 +12,45 @@ type person struct {
 	lname string
 }
 
+const (
+	maxLength = 20
+)
+
+func (n *person) Set(first string, last string) {
+	n.fname = first
+	n.lname = last
+	if len(first) > maxLength {
+		n.fname = first[:maxLength]
+	}
+	if len(last) > maxLength {
+		n.lname = last[:maxLength]
+	}
+}
+
 func main() {
 
 	persons := make([]person, 0)
-	newPerson := person{fname: "Tanzim", lname: "Mokammel"}
-	persons = append(persons, newPerson)
-	fmt.Println(persons)
 	fmt.Println("Please provide filename")
 	var filename string
 	fmt.Scanln(&filename)
-	fmt.Println(filename)
 	f, err := os.Open(filename)
 	if err != nil {
 		panic(err)
 	}
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		fullname := scanner.Text()
+		names := strings.Split(fullname, " ")
+		newPerson := &person{}
+		newPerson.Set(names[0], names[1])
+		persons = append(persons, *newPerson)
+
 	}
+
+	for _, p := range persons {
+		fmt.Println(p.fname, p.lname)
+	}
+
 	f.Close()
 
 }
