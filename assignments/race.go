c3b334eba7889be55303498fa3f79479ca39cef9
Tanzim Mokammel <tanzim.mokammel@moonfare.com>
2020-10-29 16:24:54 +0100

    moore

--- assignments/race.go
+++ assignments/race.go
@@ -1,3 +1,36 @@
+/*
+A race condition is when the outcome of your program is dependent on the non-deterministic nature of interleaving.
+This is almost never desired.
+Synchronization mechanisms are generally in place to prevent race conditions.
+An example of a race condition can be seen from the output of the provided script.
+
+In the script, we have a shared pointer to x, and two goroutines modify it concurrently.
+Therefore, the output in the main routine is dependent on the scheduler, and thus non-deterministic.
+
+Expect:  12
+Actual:  12
+Expect:  12
+Actual:  12
+Expect:  12
+Actual:  0
+Expect:  12
+Actual:  12
+Expect:  12
+Actual:  12
+Expect:  12
+Actual:  3
+Expect:  12
+Actual:  12
+Expect:  12
+Actual:  3
+Expect:  12
+Actual:  12
+Expect:  12
+Actual:  12
+Expect:  12
+Actual:  12
+*/
+
 package main
 
 import (
