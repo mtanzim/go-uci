00984a21503515c804e56e72ab8134a98661179a
Tanzim Mokammel <tanzim.mokammel@moonfare.com>
2020-11-03 12:11:31 +0100

    dining philosophers async galore

--- assignments/diningPhilosophers.go
+++ assignments/diningPhilosophers.go
@@ -3,7 +3,14 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
+
+const NumMeals = 3
+const NumChops = 5
+const NumPhilos = 5
+const NumHosts = 1
+const HostSwitchDelayMs = 250
 
 type ChopS struct{ sync.Mutex }
 
@@ -13,24 +20,86 @@ type Philo struct {
 	id      int
 }
 
-const NumMeals = 3
-const NumChops = 5
-const NumPhilos = 5
+type Host struct {
+	whoCanEat map[int]bool
+	mapLock   *sync.Mutex
+	whoIsDone chan int
+}
 
-func (p Philo) eat(wg *sync.WaitGroup) {
+func (h *Host) canPhiloEat(philoId int) bool {
+	h.mapLock.Lock()
+	canPhiloEat := h.whoCanEat[philoId]
+	h.mapLock.Unlock()
+	return canPhiloEat
+}
+
+func (h *Host) runHost(wg *sync.WaitGroup) {
 	defer wg.Done()
-	// NOTE: make this an infinite loop to see the deadlock
-	for i := 0; i < NumMeals; i++ {
-		p.leftCS.Lock()
-		p.rightCS.Lock()
-		fmt.Println("Starting to eat ", p.id)
-		fmt.Println("Finishing eating", p.id)
-		p.leftCS.Unlock()
-		p.rightCS.Unlock()
+	reset := func() {
+		for j := 0; j < NumPhilos; j++ {
+			h.whoCanEat[j+1] = false
+		}
+	}
+	fmt.Println("Hello philosophers, I am the host")
+	i := 0
+	countDone := 0
+	for countDone < NumPhilos {
+		h.mapLock.Lock()
+		switch i {
+		case 0:
+			reset()
+			h.whoCanEat[1] = true
+			h.whoCanEat[3] = true
+		case 1:
+			reset()
+			h.whoCanEat[2] = true
+			h.whoCanEat[5] = true
+		case 2:
+			reset()
+			h.whoCanEat[4] = true
+		default:
+			i = -1
+		}
+		h.mapLock.Unlock()
+
+		select {
+		case finishedEating := <-h.whoIsDone:
+			fmt.Printf("Philosopher %d is done eating!\n", finishedEating)
+			countDone++
+		default:
+			// no-op
+		}
+		i++
+		time.Sleep(HostSwitchDelayMs * time.Millisecond)
 	}
 }
 
-func initPhilos() ([]*ChopS, []*Philo) {
+func (p Philo) eat(wg *sync.WaitGroup, host *Host) {
+	signalDone := func() {
+		host.whoIsDone <- p.id
+
+	}
+	defer signalDone()
+	defer wg.Done()
+
+	fmt.Println("Hello, I am philo:", p.id)
+	i := 0
+	for i < NumMeals {
+		if host.canPhiloEat(p.id) {
+			p.leftCS.Lock()
+			p.rightCS.Lock()
+			fmt.Printf("Philosopher %d is starting to eat meal %d \n", p.id, i)
+			fmt.Printf("Philosopher %d is finishing eating meal %d \n", p.id, i)
+			i++
+			p.leftCS.Unlock()
+			p.rightCS.Unlock()
+		} else {
+			// fmt.Println("Waiting to eat ", p.id)
+		}
+	}
+}
+
+func makePhilos() []*Philo {
 
 	CSticks := make([]*ChopS, NumChops)
 	for i := 0; i < NumChops; i++ {
@@ -39,23 +108,31 @@ func initPhilos() ([]*ChopS, []*Philo) {
 	philos := make([]*Philo, NumPhilos)
 	for i := 0; i < NumPhilos; i++ {
 		philos[i] = &Philo{CSticks[i], CSticks[(i+1)%5], i + 1}
-	}
 
-	return CSticks, philos
+	}
+	return philos
+}
+
+func makeHost() *Host {
+	whoCanEat := make(map[int]bool)
+	for j := 0; j < NumPhilos; j++ {
+		whoCanEat[j+1] = false
+	}
+	var mapLock sync.Mutex
+	whoIsDone := make(chan int, NumPhilos)
+	host := Host{whoCanEat, &mapLock, whoIsDone}
+	return &host
 }
 
 func main() {
-	fmt.Println("Hello philos")
-	_, philos := initPhilos()
-	for _, p := range philos {
-		fmt.Println("Hello, I am philo:", p.id)
-	}
-	var wg sync.WaitGroup
+	philos := makePhilos()
+	host := makeHost()
 
-	wg.Add(NumPhilos)
+	var wg sync.WaitGroup
+	wg.Add(NumPhilos + NumHosts)
+	go host.runHost(&wg)
 	for i := 0; i < NumPhilos; i++ {
-		go philos[i].eat(&wg)
+		go philos[i].eat(&wg, host)
 	}
 	wg.Wait()
-
 }
